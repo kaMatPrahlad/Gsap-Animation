1513f60d8897800f96362b03f45c0654ef37e944
KaMat Prahlad <kamatprahlad43@gmail.com>
2024-05-20 15:22:31 +0530

    git init

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Highlights from './components/Highlights'
+import VideoCarousal from './components/VideoCarousel.jsx'
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <Highlights />
+      <VideoCarousal />
 
     </main>
   )
