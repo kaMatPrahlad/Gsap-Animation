388b5abc23326ccbc1be9da52de5d7262b3eafc4
KaMat Prahlad <kamatprahlad43@gmail.com>
2024-05-20 14:56:55 +0530

    git init

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Highlights = () => {
   return (
-    <section></section>)
+    <section id='highlights' className='w-screen overflow-hidden h-full comman-padding bg-zinc'>
+      <div className='screen-max-width'></div>
+      <div className=''>
+        <h1></h1>
+
+      </div>
+
+    </section>)
 }
 
 export default Highlights
