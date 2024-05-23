36b3ddda3fc042bfa4f58d7779858b5e310a0efb
KaMat Prahlad <kamatprahlad43@gmail.com>
2024-05-23 14:06:41 +0530

    git init

--- src/main.jsx
+++ src/main.jsx
@@ -3,27 +3,27 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 //...
-import * as Sentry from "@sentry/react";
+// import * as Sentry from "@sentry/react";
 
-Sentry.init({
-  dsn: "https://fd852003d8d0bba2a42ef33b23f65b59@o4507300117807104.ingest.us.sentry.io/4507300122591232",
-  integrations: [
-    Sentry.browserTracingIntegration(),
-    Sentry.metrics.metricsAggregatorIntegration(),
-    Sentry.reactRouterV6BrowserTracingIntegration({
-      useEffect: React.useEffect,
-    }),
-    Sentry.replayIntegration({
-      maskAllText: false,
-      blockAllMedia: false,
-    }),
-  ],
+// Sentry.init({
+//   dsn: "https://fd852003d8d0bba2a42ef33b23f65b59@o4507300117807104.ingest.us.sentry.io/4507300122591232",
+//   integrations: [
+//     Sentry.browserTracingIntegration(),
+//     Sentry.metrics.metricsAggregatorIntegration(),
+//     Sentry.reactRouterV6BrowserTracingIntegration({
+//       useEffect: React.useEffect,
+//     }),
+//     Sentry.replayIntegration({
+//       maskAllText: false,
+//       blockAllMedia: false,
+//     }),
+//   ],
 
-  tracesSampleRate: 1.0,
-  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
-  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-});
+//   tracesSampleRate: 1.0,
+//   tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+//   replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+//   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+// });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
