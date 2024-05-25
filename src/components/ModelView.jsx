ea7549cf9f4a9dc4e43f4fa76be5e75f70371057
KaMat Prahlad <kamatprahlad43@gmail.com>
2024-05-25 18:31:28 +0530

    git init

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -3,7 +3,7 @@ import { Html, OrbitControls, PerspectiveCamera, View } from "@react-three/drei"
 import * as THREE from 'three'
 import Lights from './Lights';
 import Loader from './Loader';
-import IPhone from './IPhone';
+import Phone from './Phone';
 import { Suspense } from "react";
 
 const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationState, size, item }) => {
@@ -32,7 +32,7 @@ const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationState, si
 
       <group ref={groupRef} name={`${index === 1} ? 'small' : 'large`} position={[0, 0 ,0]}>
         <Suspense fallback={<Loader />}>
-          <IPhone 
+          <Phone 
             scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
             item={item}
             size={size}
