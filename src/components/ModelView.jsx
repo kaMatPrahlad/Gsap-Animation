345d48849ea80b08b20eb1cae0593196ce75e2e1
KaMat Prahlad <kamatprahlad43@gmail.com>
2024-05-25 16:42:00 +0530

    git commit

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -8,7 +8,7 @@ import {
 import * as THREE from "three";
 import Lights from "./Lights";
 import Loader from "./Loader";
-import IPhone from "./Iphone";
+import IPhone from "./IPhone";
 import { Suspense } from "react";
 
 const ModelView = ({
