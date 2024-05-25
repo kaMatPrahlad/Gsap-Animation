03bdde2467637c8b5162bf847b2344c5c4cc3164
KaMat Prahlad <kamatprahlad43@gmail.com>
2024-05-25 16:32:44 +0530

    git init

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -8,7 +8,7 @@ import {
 import * as THREE from "three";
 import Lights from "./Lights";
 import Loader from "./Loader";
-import IPhone from "./Iphone";
+// import IPhone from "./Iphone";
 import { Suspense } from "react";
 
 const ModelView = ({
@@ -49,11 +49,11 @@ const ModelView = ({
         position={[0, 0, 0]}
       >
         <Suspense fallback={<Loader />}>
-          <IPhone
+          {/* <IPhone
             scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
             item={item}
             size={size}
-          />
+          /> */}
         </Suspense>
       </group>
     </View>
